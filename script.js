774696f90d38a64577a9b60c8815b39d653c91d4
UltraVoron <chavarriasng01@gmail.com>
2025-03-11 20:30:39 +0300

    Added GitHub link

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const darkModeToggle = document.getElementById('darkModeToggle');
     const body = document.body;
     const searchInput = document.getElementById('searchInput');
+    const githubIcon = document.getElementById('githubIcon');
 
     // dark mode logic
     const isDarkMode = localStorage.getItem('darkMode') === 'true';
@@ -29,6 +30,13 @@ document.addEventListener('DOMContentLoaded', () => {
                     card.style.display = 'none';
                 }
             });
+        });
+    }
+
+    // GitHub
+    if (githubIcon) {
+        githubIcon.addEventListener('click', () => {
+            window.location.href = 'https://github.com/curvedhammer/PPG-mods-library';
         });
     }
 });
